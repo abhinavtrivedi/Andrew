40d8fb18264b7a3560a58bada948557ebff222f3
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2013-06-05 01:06:04 -0700

    location service working

--- src/com/idletimeout/andrewapp/LocationUtils.java
+++ src/com/idletimeout/andrewapp/LocationUtils.java
@@ -1,10 +1,45 @@
 package com.idletimeout.andrewapp;
 
+import android.content.Context;
+import android.location.Location;
+
 public class LocationUtils {
 	
+	// Debugging tag for the application
+    public static final String APPTAG = "AndrewSample";
+    
 	// Location update interval 
 	protected static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5000L;
 	
 	// Fast ceiling in milliseconds
 	protected static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = 60000L;
+	
+	// Name of shared preferences repository that stores persistent state
+    public static final String SHARED_PREFERENCES =
+            "com.idleTimeout.andrewapp.SHARED_PREFERENCES";
+    
+    // Create an empty string for initializing strings -- This is a good idea, saves some memory
+    public static final String EMPTY_STRING = new String();
+    
+    /**
+     * Get the latitude and longitude from the Location object returned by
+     * Location Services.
+     *
+     * @param currentLocation A Location object containing the current location
+     * @return The latitude and longitude of the current location, or null if no
+     * location is available.
+     */
+    protected static String getLatLng(Context context, Location currentLocation) {
+        // If the location is valid
+        if (currentLocation != null) {
+            // Return the latitude and longitude as strings
+            return context.getString(
+                    R.string.latitude_longitude,
+                    currentLocation.getLatitude(),
+                    currentLocation.getLongitude());
+        } else {
+            // Otherwise, return the empty string
+            return EMPTY_STRING;
+        }
+    }
 }
--- src/com/idletimeout/andrewapp/MainActivity.java
+++ src/com/idletimeout/andrewapp/MainActivity.java
@@ -1,15 +1,28 @@
 package com.idletimeout.andrewapp;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -44,7 +57,11 @@ public class MainActivity extends FragmentActivity implements
 		setContentView(R.layout.activity_main);
 		
 		// UI view objects
-		mCoordinates = (TextView) findViewById(R.id.label_coordinates);
+		mCoordinates = (TextView) findViewById(R.id.coordinates);
+		mAddress = (TextView) findViewById(R.id.address);
+		mActivityIndicator = (ProgressBar) findViewById(R.id.location_progress);
+		mConnectionState = (TextView) findViewById(R.id.text_connection_state);
+		mConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
 		
 		// create Location Object
 		mLocationRequest  = LocationRequest.create();
@@ -58,8 +75,30 @@ public class MainActivity extends FragmentActivity implements
 		// set interval ceiling
 		mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
 		
+		// open shared pref
+		mPrefs = getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+		
+		// get an editor
+		mEditor = mPrefs.edit();
+		
 		// create location client
 		mLocationClient = new LocationClient(this, this, this);
+	}
+	
+	private boolean servicesConnected() {
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		
+		if(ConnectionResult.SUCCESS == resultCode) {
+			return true;
+		} else {
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+			if(dialog != null) {
+				ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+				dialogFragment.setDialog(dialog);
+				dialogFragment.show(getSupportFragmentManager(), LocationUtils.APPTAG);
+			}
+			return false;
+		}
 	}
 	
 	
@@ -82,8 +121,8 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
 		super.onStart();
+		mLocationClient.connect();
 	}
 
 
@@ -92,8 +131,9 @@ public class MainActivity extends FragmentActivity implements
 	protected void onStop() {
 		if(mLocationClient.isConnected()) {
 			mLocationClient.removeLocationUpdates(this);
-			
+			mConnectionState.setText(R.string.label_unknown);
 		}
+		mLocationClient.disconnect();
 		super.onStop();
 	}
 
@@ -114,19 +154,113 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	public void onConnected(Bundle connectionHint) {
-		// TODO Auto-generated method stub
-		
+		mConnectionStatus.setText(R.string.label_connected);
 	}
 
 	@Override
 	public void onDisconnected() {
-		// TODO Auto-generated method stub
+		mConnectionStatus.setText(R.string.label_disconnected);
 		
 	}
 
 	@Override
 	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * get_location_button
+	 * @param v
+	 */
+	public void getLocation(View v) {
+		if (servicesConnected()) {
+			Location currentLocation = mLocationClient.getLastLocation();
+			mCoordinates
+					.setText(LocationUtils.getLatLng(this, currentLocation));
+		}
+	}
+	
+	/**
+	 * get_address_button
+	 * @param v
+	 */
+	public void getAddress(View v) {
+		if (servicesConnected()) {
+			Location currentLocation = mLocationClient.getLastLocation();
+			mActivityIndicator.setVisibility(View.VISIBLE);
+			new GetAddressTask(this).execute(currentLocation);
+		} 
+	}
+	
+	/**
+	 * Nested class to display error dialog
+	 * @author abhi
+	 *
+	 */
+	@SuppressLint("NewApi")
+	protected static class ErrorDialogFragment extends DialogFragment {
+		private Dialog mDialog;
+
+		public ErrorDialogFragment() {
+			super();
+			this.mDialog = null;
+		}
+
+		public void setDialog(Dialog dialog) {
+			this.mDialog = dialog;
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	
+	}
+	
+	protected class GetAddressTask extends AsyncTask<Location, Void, String> {
+		
+		Context context;
+		
+		public GetAddressTask(Context context) {
+			super();
+			this.context = context;
+		}
+
+		@Override
+		protected String doInBackground(Location... params) {
+			
+			Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+			
+			Location location = params[0];
+			
+			List<Address> addresses = null;
+			
+			try {
+				addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+			} catch (Exception e) {
+				return getString(R.string.label_unknown);
+			}
+			
+			if(addresses != null && addresses.size() > 0) {
+				Address address = addresses.get(0);
+				String line1 = address.getMaxAddressLineIndex()>0 ? address.getAddressLine(0) : address.getThoroughfare();
+				String line2 = address.getMaxAddressLineIndex()>1 ? address.getAddressLine(1) : address.getLocality();
+				String line3 = address.getMaxAddressLineIndex()>2 ? address.getAddressLine(2) : address.getAdminArea();
+				String addressString = getString(R.string.address_output_string,line1, line2, line3);
+				return addressString;
+			} else {
+				return getString(R.string.label_unknown);
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			mActivityIndicator.setVisibility(View.GONE);
+			mAddress.setText(result);
+		}
+		
+		
 		
 	}
 
